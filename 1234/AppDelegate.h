c4dd1a33997a95a3c2399be9831838dc9fb4e5f5
BingGe123 <mrli_bing@163.com>
2020-01-10 14:24:04 +0800

    测试

---
+++ 1234/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1234
+//
+//  Created by 李冰 on 2019/4/20.
+//  Copyright © 2019 李冰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1234/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1234
+//
+//  Created by 李冰 on 2019/4/20.
+//  Copyright © 2019 李冰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
